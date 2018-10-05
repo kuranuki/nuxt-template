e6de8d2469e29e15786595aed792cb3c55ee7778
kuranuki <kuranuki@sonicgarden.jp>
2018-10-05 13:08:17 +0900

    初期メソッド状態アレンジ

--- template/store/index.ts
+++ template/store/index.ts
@@ -1,5 +1,20 @@
-import { firebaseMutations } from 'vuexfire'
+import { firebaseMutations, firebaseAction } from 'vuexfire'
+// import firestore from '~/plugins/firestore'
+
+// const todoListsRef = firestore.collection('trello-lists')
 
 export const mutations = {
   ...firebaseMutations
+}
+
+export const state = () => ({
+  // todoLists: []
+})
+
+export const actions = {
+  init: firebaseAction(({ bindFirebaseRef }) => {
+    // bindFirebaseRef('todoLists', todoListsRef.orderBy('timestamp', 'desc'))
+    //   .then(() => console.log('success binded'))
+    //   .catch(err => console.log(err))
+  })
 }
